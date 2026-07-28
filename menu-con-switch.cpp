e15dc19e53c2ae08eb9fb9a96f519f56a1b2af34
Belén <coisaok.contacto@gmail.com>
2026-07-28 19:16:42 -0300

    Refactor menu and function declaration
    Refactor menu and function declaration for clarity.

--- menu-con-switch.cpp
+++ menu-con-switch.cpp
@@ -4,14 +4,51 @@ Mostrar un Menú
 con 4 operaciones matemáticas
 Pedir dos números
 y aplicar la operación elejida.
-
 */
-
-
 #include <iostream>
 using namespace std;
 
  //Declarar funcion
+float operar(float num1,float num2,int operacion);
+
+int main(){   
+    
+    float num1,num2;
+    int operacion;
+    
+    //Menú 
+    cout<< "Menú de operaciones:"<<endl<<endl
+        <<"1-Suma"<<endl
+        <<"2-Resta"<<endl
+        <<"3-Multiplicación"<<endl
+        <<"4-División"<<endl<<endl;
+    
+    //Solicitar dos números
+    cout<< "Ingrese primer número: "<<endl;
+    cin>>num1;
+    cout<<"Ingrese segundo número: "<<endl;
+    cin>>num2;
+    
+    //Solicitar operación
+    cout <<"Elija la operación a realizar: "<<endl;
+    cin>>operacion;
+    
+    
+    //Declarar y definir variable resultado
+    float resultado = operar(num1,num2,operacion);
+    
+    //Ejecutar función si la operacion es válida
+    if (operacion == 4 && num2 ==0){
+        //Error del Switch case 4
+    } else if (operacion < 1 || operacion >4){
+      //Error default del Switch  
+    } else {
+        cout<<"El resultado de la operación elejida es: "<<resultado<<endl;
+    }
+    return 0;
+}
+
+//Definir función
 float operar(float num1,float num2,int operacion) {
     switch(operacion){
         case 1:
@@ -31,39 +68,4 @@ float operar(float num1,float num2,int operacion) {
             cout<<"Hubo un error."<<endl;
             return 0;
     }
-}
-
-int main(){   
-    
-    float num1,num2;
-    int operacion;
-    
-    //Armar menu con 4 operaciones matematicas y mostrar 
-    cout<< "Menú de operaciones:"<<endl<<endl
-        <<"1-Suma"<<endl
-        <<"2-Resta"<<endl
-        <<"3-Multiplicación"<<endl
-        <<"4-División"<<endl<<endl;
-    
-    //Pedir dos numeros
-    cout<< "Ingrese primer número: "<<endl;
-    cin>>num1;
-    cout<<"Ingrese segundo número: "<<endl;
-    cin>>num2;
-    
-    //Pedir operacion
-    cout <<"Elija la operación a realizar: "<<endl;
-    cin>>operacion;
-    
-    
-    //Declaro y defino variable resultado
-    float resultado = operar(num1,num2,operacion);
-    
-    //Ejecutar función si la operacion es valida
-    if (operacion == 4 && num2 ==0){
-        //Error del Switch case 4
-    } else {
-        cout<<"El resultado de la operación elejida es: "<<resultado<<endl;
-    }
-    return 0;
 }
